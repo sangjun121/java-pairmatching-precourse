3d2d9c7665dbe114f5ba9ef1cf36008bbf609448
조상준 <juncho12011201@gmail.com>
2026-01-05 16:06:35 +0900

    feat(domain): 과정, 레벨, 미션 기본 정보 어플리케이션 등록

---
+++ src/main/java/pairmatching/domain/Mission.java
@@ -0,0 +1,28 @@
+package pairmatching.domain;
+
+public enum Mission {
+    RACING_CAR(Level.LEVEL1, "자동차경주"),
+    LOTTO(Level.LEVEL1, "로또"),
+    BASEBALL(Level.LEVEL1, "숫자야구게임"),
+    CART(Level.LEVEL2, "장바구니"),
+    PAYMENT(Level.LEVEL2, "결제"),
+    SUBWAY(Level.LEVEL2, "지하철노선도"),
+    PERFORMANCE(Level.LEVEL4, "성능개선"),
+    DEPLOY(Level.LEVEL4, "배포");
+
+    private final Level level;
+    private final String name;
+
+    Mission(Level level, String name) {
+        this.level = level;
+        this.name = name;
+    }
+
+    public Mission getMissionByName(String name){
+        for(Mission mission : Mission.values()){
+            if (mission.name.equals(name)) {
+                return mission;
+            }
+        }
+    }
+}
